16d97741840fd3c02805b7018a7d6ed56764125f
manjunath <manjunath7068@gmail.com>
2022-05-23 14:53:38 +0530

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by manjunath on 23/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
